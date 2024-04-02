ae6e26c7ff4754cbae4851c4564d0e087bf01b11
Yujiro Yukawa <yuj.yuk@icloud.com>
2024-04-02 18:34:17 +0900

    課題提出

---
+++ src/practice/Task.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.time.LocalDate;
+
+public class Task {
+	private LocalDate date;
+    private String content;
+
+    public Task(LocalDate date, String content) {
+        this.date = date;
+        this.content = content;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
